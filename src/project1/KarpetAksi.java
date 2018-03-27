883eab75e583d96a5bdc3cb342099a625074ddd4
MasyudiPutra <32509043+MasyudiPutra@users.noreply.github.com>
2018-03-27 12:39:22 +0800

    memanggil method

--- src/project1/KarpetAksi.java
+++ src/project1/KarpetAksi.java
@@ -11,7 +11,15 @@ package project1;
  */
 public class KarpetAksi {
     public static void main(String[] args) {
+        Karpet k1 = new Karpet();
         
+        k1.merek = "Orchid";
+        k1.bahan = "Karet";
+        k1.harga = 15000;
+        k1.made = "Korea";
+        k1.kualitas = "Baik";
+        
+        k1.infoKarpet();
     }
     
 }
